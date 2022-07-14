d83d1e04c824da41147d02c55277e2d162f73442
Stanislav Kuznietsov <bisu.stats@gmail.com>
2022-07-14 13:01:19 +0300

    Del Post

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import PostItem from "./PostItem";
 
-const PostList = ({posts, title}) => {
+const PostList = ({posts, title, remove}) => {
     return ( /* map для каждого поста отрисовывает post item и как props передаем туда обьект */
         <div>
             <h1>{title}</h1>
-            {posts.map((post, index) =>  <PostItem number={index +1} post={post} key={post.id}/> )}
+            {posts.map((post, index) =>  <PostItem remove={remove} number={index +1} post={post} key={post.id}/> )}
 
         </div>/* к каждому посту приклеиваем дату в качестве уникального номера  */
     );
